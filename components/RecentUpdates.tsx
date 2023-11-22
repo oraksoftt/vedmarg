5cb81a14c4533577d0e4b89c8f35dc582fc1653c
MatiullahZahir <97434191+MatiullahZahir@users.noreply.github.com>
2023-11-22 10:17:25 +0400

    images issue resolved

--- components/RecentUpdates.tsx
+++ components/RecentUpdates.tsx
@@ -1,46 +1,35 @@
- 
-'use client'
-import { useEffect, useState } from 'react';
+"use client"; 
 
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-import { RECENT_UPDATES } from '@/constants';
-import Image from 'next/image';
+import { RECENT_UPDATES } from "@/constants";
+import Image from "next/image";
 
 const RecentUpdates = () => {
-  const [slidesToShow, setSlidesToShow] = useState(3);
-
-  useEffect(() => {
-    // Update slidesToShow based on screen size
-    const updateSlidesToShow = () => {
-      if (window.innerWidth < 640) {
-        setSlidesToShow(1); // Small screens
-      } else if (window.innerWidth < 1024) {
-        setSlidesToShow(2); // Medium screens
-      } else {
-        setSlidesToShow(3); // Large screens
-      }
-    };
-
-    updateSlidesToShow();
-
-    // Event listener for window resize
-    window.addEventListener('resize', updateSlidesToShow);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener('resize', updateSlidesToShow);
-    };
-  }, []);
-
+  
   const settings = {
     dots: true,
     infinite: true,
     speed: 200,
-    slidesToShow: slidesToShow,
-    slidesToScroll: 1,  
+    slidesToShow:3,// slidesToShow,
+    slidesToScroll: 1,
+    autoplay: true, // Enable autoplay
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -54,19 +43,20 @@ const RecentUpdates = () => {
       <div className="max-container padding-container relative w-full flex justify-end">
         <div className="z-20 flex w-full flex-col lg:w-[100%]">
           <ul className="mt-10 gap-10 md:grid-cols-2 lg:grid-cols-3 lg:gap-20">
-            <Slider {...settings}>
+            <Slider {...settings} >
               {RECENT_UPDATES.map((value, index) => (
                 <div key={index} className="py-2 pl-2 pr-2">
                   <Image
                     src={value.image}
                     alt={value.title}
-                    width={500}
-                    height={500}
-                    className="h-full w-full object-cover"
+                    width={1000}
+                    height={600}
+                    className="h-full w-full object-cover     transition duration-300 ease-in-out hover:opacity-40"
                   />
                   <p className="text-lg lg:text-2xl mt-3 capitalize">
                     {value.title}
                   </p>
+                   
                 </div>
               ))}
             </Slider>
